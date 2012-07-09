8893614223c991f1c24171cc8d3116d9aa679f24
U-proxa-PC\proxa <proxa@proxa-PC.(none)>
2012-07-09 14:20:41 -0500

    Implemented MySQL connectivity and logging, fixed permissions bug with OPs.

--- src/org/seed419/founddiamonds/Node.java
+++ src/org/seed419/founddiamonds/Node.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 
 import java.util.List;
+import org.bukkit.inventory.ItemStack;
 
 /**
  * Created with IntelliJ IDEA.
@@ -15,12 +16,17 @@ import java.util.List;
 public class Node {
 
 
-    private Material mat;
+    private ItemStack item;
     private ChatColor color;
 
 
     public Node(Material mat, ChatColor color) {
-        this.mat = mat;
+        this.item = new ItemStack(mat);
+        this.color = color;
+    }
+    
+    public Node(ItemStack stack, ChatColor color) {
+        this.item = stack;
         this.color = color;
     }
 
@@ -29,46 +35,56 @@ public class Node {
     }
 
     public Material getMaterial() {
-        return mat;
+        return item.getType();
     }
 
     public static Node parseNode(String cb) {
-        String[] bi = cb.split(":");
-//TODO Parse item IDs as well!
+        String[] bi = cb.split(",");
         //Parse the material string
-        Material matchAttempt = parseMaterial(bi[0]);
+        ItemStack matchAttempt = parseItemStack(bi[0]);
         if (matchAttempt == null) {
             return null;
         }
-        if (matchAttempt.isBlock()) {
-            ChatColor color = null;
-            if (bi.length == 1) {
-                // No color specified - attempt to get default
-                color = BlockColor.getBlockColor(matchAttempt);
-            } else if (bi.length == 2) {
-                // parse the color
-                color = parseChatColor(bi[1]);
-                if (color == null) {
-                    return null;
-                }
+        if (bi.length < 2) {
+            if (matchAttempt.getType() == null) {
+                return new Node(matchAttempt, ChatColor.WHITE);
             }
+            if (!matchAttempt.getType().isBlock()) return null;
+            ChatColor color = null;
+            // No color specified - attempt to get default
+            color = BlockColor.getBlockColor(matchAttempt.getType());
             return new Node(matchAttempt, color);
         }
-        return null;
+        else {
+            // parse the color
+            ChatColor color = parseChatColor(bi[1]);
+            if (color == null) return null;
+            return new Node(matchAttempt, color);
+        }
     }
 
 
-    public static Material parseMaterial(String matName) {
-        Material matchAttempt;
-        try {
-            int id = Integer.parseInt(matName);
-            matchAttempt = Material.getMaterial(id);
-        } catch (NumberFormatException ex) {
-            matchAttempt = Material.matchMaterial(matName);
-        } catch (Exception ex) {
-            return null;
+    public static ItemStack parseItemStack(String matName) {
+        Material matchMaterial = Material.matchMaterial(matName);
+        if (matchMaterial != null) return new ItemStack(matchMaterial);
+        String[] split = matName.split(":");
+        if (split.length < 2) {
+            try {
+                int id = Integer.parseInt(split[0]);
+                return new ItemStack(id, 1);
+            } catch (NumberFormatException ex) {
+                return null;
+            }
         }
-        return matchAttempt;
+        else {
+            try {
+                int id = Integer.parseInt(split[0]);
+                short damage = (short) Integer.parseInt(split[1]);
+                return new ItemStack(id, 1, damage);
+            } catch (NumberFormatException ex) {
+                return null;
+            }
+        }
     }
 
     public static ChatColor parseChatColor(String colorName) {
